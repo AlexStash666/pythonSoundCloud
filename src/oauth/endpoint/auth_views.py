58eb053adb54b0a6308be287454cafafa74e4062
AlexStash666 <alexanderstashinski@gmail.com>
2022-01-16 18:12:19 +0300

    spotify login

--- src/oauth/endpoint/auth_views.py
+++ src/oauth/endpoint/auth_views.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 
 from .. import serializer
-from ..services.google import check_google_auth
+from ..services import google, spotify
 
 
 def google_login(request):
@@ -12,12 +12,24 @@ def google_login(request):
     return render(request, 'oauth/google_login.html')
 
 
+def spotify_login(request):
+    """Страница входа через spotify"""
+    return render(request, 'oauth/spotify_login.html')
+
+
 @api_view(["POST"])
 def google_auth(request):
     """Подтверждение авторизации через google"""
     google_data = serializer.GoogleAuth(data=request.data)
     if google_data.is_valid():
-        token = check_google_auth(google_data.data)
+        token = google.check_google_auth(google_data.data)
         return Response(token)
     else:
         return AuthenticationFailed(code=403, detail='Bad data Google')
+
+
+@api_view(["GET"])
+def spotify_auth(request):
+    """Подтверждение авторизации через Spotify"""
+    token = spotify.spotify_auth(request.query_params.get('code'))
+    return Response(token)
