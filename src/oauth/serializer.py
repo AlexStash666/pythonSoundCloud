c3018d2e36c70dd2a52c2f423b4ed391d1c030b5
AlexStash666 <alexanderstashinski@gmail.com>
2022-01-26 14:45:21 +0300

    permissions

--- src/oauth/serializer.py
+++ src/oauth/serializer.py
@@ -13,3 +13,18 @@ class GoogleAuth(serializers.Serializer):
     """Сериализация данных гугл"""
     email = serializers.EmailField()
     token = serializers.CharField()
+
+
+class SocialLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SocialLink
+        fields = ('link',)
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    social_links = SocialLinkSerializer(many=True)
+
+    class Meta:
+        model = models.AuthUser
+        fields = (
+        'id', 'avatar', 'country', 'city', 'bio', 'display_name', 'social_links')
--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -4,8 +4,12 @@ from .endpoint import views, auth_views
 urlpatterns = [
     path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
 
+    path('author/', views.AuthorView.as_view({'get': 'list'})),
+    path('author/<int:pk>/', views.AuthorView.as_view({'get': 'retrieve'})),
     path('spotify-callback/', auth_views.spotify_auth),
-
+    path('social/', views.SocialLinkView.as_view({'get': 'list', 'post': 'create'})),
+    path('social/<int:pk>/',
+         views.SocialLinkView.as_view({'put': 'update', 'delete': 'destroy'})),
     path('spotify-login/', auth_views.spotify_login),
 
     path('google/', auth_views.google_auth),
